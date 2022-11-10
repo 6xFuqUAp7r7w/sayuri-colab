cd659330829d26a680c8cdc2202930cf309ed7a7
matobataketoshi <54569734+matobataketoshi@users.noreply.github.com>
2022-11-10 19:19:52 +0900

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,22 +1,22 @@
 set GOOS=darwin
 set GOARCH=amd64
-go build -o ./bin/colab-katago-gd-for-mac 
+go build -o ./bin/colab-sayuri-gd-for-mac 
 
 set GOOS=linux
 set GOARCH=amd64
-go build -o ./bin/colab-katago-gd-for-linux
+go build -o ./bin/colab-sayuri-gd-for-linux
 
 set GOOS=windows
 set GOARCH=amd64
-go build -o ./bin/colab-katago-gd-for-windows
+go build -o ./bin/colab-sayuri-gd-for-windows
 
 pushd bin
 del /q *.zip
-copy /y colab-katago-gd-for-mac colab-katago-gd
-tar -a -c -f colab-katago-gd.mac.zip colab-katago-gd
-copy /y colab-katago-gd-for-linux colab-katago-gd
-tar -a -c -f colab-katago-gd.linux.zip colab-katago-gd
-copy /y colab-katago-gd-for-windows colab-katago-gd.exe
-tar -a -c -f colab-katago-gd.windows.zip colab-katago-gd.exe
-del /q colab-katago-gd colab-katago-gd.exe
+copy /y colab-sayuri-gd-for-mac colab-sayuri-gd
+tar -a -c -f colab-sayuri-gd.mac.zip colab-sayuri-gd
+copy /y colab-sayuri-gd-for-linux colab-sayuri-gd
+tar -a -c -f colab-sayuri-gd.linux.zip colab-sayuri-gd
+copy /y colab-sayuri-gd-for-windows colab-sayuri-gd.exe
+tar -a -c -f colab-sayuri-gd.windows.zip colab-sayuri-gd.exe
+del /q colab-sayuri-gd colab-sayuri-gd.exe
 popd
